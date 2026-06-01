9a469dde5605d67cf13f6e666ffa4ffd9670765c
tateoka <tomboy0803@yahoo.com>
2026-06-01 11:25:41 +0900

    feat: ログイン画面のエラーメッセージ・ログインボタンを実装

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Laravel\Fortify\Http\Requests\LoginRequest as FortifyLoginRequest;
+
+class LoginRequest extends FortifyLoginRequest
+{
+    public function rules(): array
+    {
+        return [
+            'email'    => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required'    => 'メールアドレスを入力してください',
+            'email.email'       => 'メールアドレスはメール形式で入力してください',
+            'password.required' => 'パスワードを入力してください',
+        ];
+    }
+}
